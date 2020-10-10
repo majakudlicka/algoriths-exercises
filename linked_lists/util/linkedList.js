6915369d2fd88c53a88fda3ca520b2ec506d5291
Maja Kudlicka <maja.kudlicka@gmail.com>
2020-10-10 16:58:14 +0100

    feat: added Linked list helper class

--- linked_lists/util/linkedList.js
+++ linked_lists/util/linkedList.js
@@ -1,7 +1,7 @@
 class Node {
     constructor(data, next = null) {
         this.data = data;
-        this.next = next;
+        this.null = null
     }
 }
 
@@ -10,30 +10,105 @@ class LinkedList {
         this.head = null;
     }
 
-    insertLast(data) {
-        const last = this.getLast();
+    insertFirst(data) {
+        const node = new Node(data);
+        node.next = this.head;
+        this.head = node;
+    }
 
-        if (last) {
-            // There are some existing nodes in our chain
-            last.next = new Node(data);
-        } else {
-            // The chain is empty!
-            this.head = new Node(data);
+    size() {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            counter++;
+            node = node.next;
         }
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
     }
 
     getLast() {
-        if (!this.head) {
-            return null;
-        }
-
         let node = this.head;
+        let prev = null;
         while (node) {
-            if (!node.next) {
-                return node;
+            node = node.next;
+            prev = node;
+        }
+        return prev;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) return;
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) return;
+        let node = this.head;
+        let prev = node;
+        while (node.next) {
+            node = node.next;
+            prev = node;
+        }
+        prev.next = null;
+    }
+
+    insertLast(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let node = this.head;
+            while (node.next) {
+                node = node.next;
             }
+            node.next = newNode;
+        }
+    }
+
+    getAt(index) {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            if (counter === index) return node;
+            counter++;
             node = node.next;
         }
+
+        return null;
+    }
+
+    removeAt(index) {
+        if (!this.head) return;
+        if (index === 0) {
+            this.head = null;
+            return;
+        }
+        let prev = this.getAt(index -1);
+        if (!prev || !prev.next) return;
+        prev.next = prev.next.next;
+
+    }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+        const prev = this.getAt(index - 1);
+        prev.next = new Node(data, prev.next);
     }
 
     _toArray() {
